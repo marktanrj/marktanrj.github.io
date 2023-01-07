7e578ae0bfec53d1d06e7b8286e8838e7d595d01
Mark Tan <marktanrj@gmail.com>
2023-01-08 02:50:37 +0800

    Update profile pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import selfie from '../assets/selfie.svg'
+import selfie from '../assets/anime_selfie.jpg'
 import { Github, Linkedin } from '../components'
 import { GITHUB_URL, LINKEDIN_URL } from '../constants'
 
